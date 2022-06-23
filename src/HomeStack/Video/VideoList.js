20c82940d8926d1df6dad283fa58b4b878d06f99
Moksha <moksha.vemula@xcubelabs.com>
2022-06-23 12:33:52 +0530

    m

---
+++ src/HomeStack/Video/VideoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View, FlatList, ScrollView, StyleSheet} from 'react-native'
+
+
+const VideoList = () =>{
+    return(
+        <View>
+            <Text>Hi</Text>
+        </View>
+    )
+}
+
+export default VideoList
